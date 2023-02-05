22e9e251afc5aa922936fe2fe6ac79aafbb263e4
William Martineau <william_martineau@hotmail.com>
2023-02-04 23:19:11 -0500

    working on android

--- FlutterApp/bethere/lib/main.dart
+++ FlutterApp/bethere/lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_database/firebase_database.dart';
